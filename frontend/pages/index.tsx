c18debddfc3fc4ad3f176ba60960c8c8dbd2266f
Dakota Schramm <william.schramm11@gmail.com>
2023-01-09 19:56:05 -0600

    Added button to navigate to game.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 
 const Home: NextPage = () => {
   return (
     // Have main screen that lets you navigate to scores, exit and play
-    <section className='w-screen h-screen'>
-      <h1>Yahtzee!</h1>
-      <h2>Written by Dakota Schramm</h2>
-
+    <section className='flex flex-col items-center justify-around w-screen h-screen bg-blue-300'>
+      <header className='flex flex-col items-center justify-center'>
+        <h1>Yahtzee!</h1>
+        <h2>Written by Dakota Schramm</h2>
+      </header>
+      <section className='flex justify-center items-between'>
+        <Link href='/game'>
+          <button
+            className='flex items-center justify-center p-4 mx-4 text-white bg-blue-500 rounded-md h-11'
+          >
+            Play
+          </button>
+        </Link>
+      </section>
     </section>
   )
 }
