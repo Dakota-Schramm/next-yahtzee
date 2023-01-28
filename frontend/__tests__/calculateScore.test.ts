b7228f1c2498ce402c0d57d29c49efa845a06afa
Dakota Schramm <william.schramm11@gmail.com>
2023-01-27 21:37:48 -0600

    More work on Scoreboard tests.

---
+++ frontend/__tests__/calculateScore.test.ts
@@ -0,0 +1,24 @@
+
+import React from 'react'
+import {render, screen} from '@testing-library/react'
+// import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import {describe, expect, test} from '@jest/globals';
+
+import { calculateScore } from '~/components/Scoreboard'
+
+const allOnes = [ ...Array(5) ].map(
+  (_) => ({ face: 1, shouldReroll: false })
+)
+
+// test that all ones returns 5
+test('all ones returns 5 for 1 Upper score', () => {
+  const outputScore = calculateScore(allOnes, 1);
+  expect(outputScore).toBe(5)
+})
+
+// test that all ones returns 0 for 2 Upper score
+test('all ones returns 0 for 2 Upper score', () => {
+  const outputScore = calculateScore(allOnes, 2);
+  expect(outputScore).toBe(0)
+})
