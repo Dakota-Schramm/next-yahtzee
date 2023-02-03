6afd184dc7a36ce9b87b7e0f74bedafdf364696a
Dakota Schramm <william.schramm11@gmail.com>
2023-02-03 16:51:42 -0600

    Added descriptions for scoreboard scores.

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,21 +1,24 @@
 export const numOfDice = 5;
 
-export const upperSectionScores = [
-  1,
-  2,
-  3,
-  4,
-  5,
-  6
-];
+interface Score {
+  [key: string | number]: string
+}
 
-export const lowerSectionScores = [
-  "3 of a kind",
-  "4 of a kind",
-  "Full House",
-  "Small Straight",
-  "Large Straight",
-  "Yahtzee!",
-  "Chance"
-];
+export const upperSectionScores: Score = {
+  1: "Sum of all ones rolled",
+  2: "Sum of all twos rolled",
+  3: "Sum of all threes rolled",
+  4: "Sum of all fours rolled",
+  5: "Sum of all fives rolled",
+  6: "Sum of all sixes rolled"
+};
 
+export const lowerSectionScores: Score ={
+  "3 of a kind": "Three dice have the same face",
+  "4 of a kind": "Four dice have the same face",
+  "Full House": "Three of a kind and two of a kind in one hand",
+  "Small Straight": "Four sequential dice - Ex. [1, 2, 3, 4]",
+  "Large Straight": "Five sequential dice - Ex. [1, 2, 3, 4, 5]",
+  "Yahtzee!": "Five of a kind - all dice have the same face",
+  "Chance": "Sum of all dice"
+}
