4031f8437e0b42cdfcf2904a75b1bbf3301a8441
Dakota Schramm <william.schramm11@gmail.com>
2024-04-18 21:42:00 -0500

    Fix React import error on game page

--- frontend/pages/game.tsx
+++ frontend/pages/game.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect, useState, FC, useLayoutEffect, useReducer, useContext } from 'react';
+import React, { useEffect, useState, FC, useLayoutEffect, useReducer, useContext } from 'react';
 
 import { upperSectionScores, lowerSectionScores } from '../constants';
 
