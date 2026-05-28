f775650ec5fe88cce10cce9c98026f757d2f9585
Dakota Schramm <william.schramm11@gmail.com>
2026-05-27 19:44:29 -0500

    Start setting up iam file for IAM user

---
+++ infra/terraform/iam.tf
@@ -0,0 +1,24 @@
+# users
+
+resource "aws_iam_user" "fine_grained_user" {
+  name = var.user_name
+}
+
+resource "aws_iam_access_key" "access_key" {
+  user = aws_iam_user.fine_grained_user.name
+}
+
+resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
+  user       = aws_iam_user.fine_grained_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+# Maybe output to env file instead??
+resource "local_file" "access_key_file" {
+  filename = var.filename
+  content = <<-EOT
+    IAM User Credentials : ${aws_iam_user.fine_grained_user.name}
+    Access Key: ${aws_iam_access_key.access_key.id}
+    Secret Key: ${aws_iam_access_key.access_key.secret}
+  EOT
+}
---
+++ infra/terraform/variables.tf
@@ -0,0 +1,13 @@
+# iam variables
+
+variable "user_name" {
+  description = "The name for your IAM user"
+  type = string
+  default = "Yahtzee_Developer"
+}
+
+variable "filename" {
+  description = "The name of the file that holds IAM user credentials"
+  type = string
+  default = "developer_access_account_info.txt"
+}
