5e291827a9681fccbda8c2d23672983afaed5f32
Dakota Schramm <william.schramm11@gmail.com>
2026-05-28 21:13:04 -0500

    Modularize iam resources

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -1,24 +1,6 @@
-# users
+module "iam" {
+  source = "./modules/iam"
 
-resource "aws_iam_user" "fine_grained_user" {
-  name = var.user_name
-}
-
-resource "aws_iam_access_key" "access_key" {
-  user = aws_iam_user.fine_grained_user.name
-}
-
-resource "aws_iam_user_policy_attachment" "admin_policy_attachment" {
-  user       = aws_iam_user.fine_grained_user.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-}
-
-# Maybe output to env file instead??
-resource "local_sensitive_file" "access_key_file" {
-  filename        = "${path.module}/${var.filename}"
-  file_permission = "0600"
-  content = <<-EOT
-    AWS_ACCESS_KEY_ID=${aws_iam_access_key.access_key.id}
-    AWS_SECRET_ACCESS_KEY=${aws_iam_access_key.access_key.secret}
-  EOT
+  user_name = var.user_name
+  filename  = var.filename
 }
---
+++ infra/terraform/modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "user_name" {
+  description = "The name for the IAM user"
+  type        = string
+}
+
+variable "filename" {
+  description = "The name of the file that holds IAM user credentials"
+  type        = string
+}
