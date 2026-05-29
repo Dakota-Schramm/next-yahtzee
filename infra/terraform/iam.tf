cf46a7019c492d2e7f7a38896de0997c8420f143
Dakota Schramm <william.schramm11@gmail.com>
2026-05-29 12:59:02 -0500

    Move module blocks into root main file

--- infra/terraform/iam.tf
+++
@@ -1,6 +0,0 @@
-module "iam" {
-  source = "./modules/iam"
-
-  user_name = var.user_name
-  filename  = var.filename
-}
--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,3 +1,20 @@
+module "iam" {
+  source = "./modules/iam"
+
+  user_name = var.user_name
+  filename  = var.filename
+}
+
+module "networking" {
+  source = "./modules/networking"
+
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  azs                  = local.azs
+  environment          = var.environment
+}
+
 provider "aws" {
   region = var.aws_region
 }
