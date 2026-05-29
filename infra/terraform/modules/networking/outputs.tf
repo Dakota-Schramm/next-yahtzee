a674c0f6ec05ee721cf461d43f5f9a3c34d0b0be
Dakota Schramm <william.schramm11@gmail.com>
2026-05-28 21:05:13 -0500

    Extract networking into own module

---
+++ infra/terraform/modules/networking/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public_subnets[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private_subnets[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the internet gateway"
+  value       = aws_internet_gateway.gw.id
+}
---
+++ infra/terraform/modules/networking/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "Availability zones to deploy subnets into"
+  type        = list(string)
+}
+
+variable "environment" {
+  description = "Environment name used for resource tags"
+  type        = string
+}
