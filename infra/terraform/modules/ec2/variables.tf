6a7ec60cc3c61c2ae118cc9a4c42a5e977715b71
Dakota Schramm <william.schramm11@gmail.com>
2026-05-29 13:55:09 -0500

    Start adding other module files for ec2

--- infra/terraform/modules/ec2/variables.tf
+++ infra/terraform/modules/ec2/variables.tf
@@ -1,10 +1,3 @@
-# NOTE:
-# These variables must be defined at root level in order to be configurable
-
-#############
-# EC2 variables
-#############
-
 variable "app_ssh_ip" {
   description = "Your IP for SSH access (e.g., 1.2.3.4/32)"
   type        = string
@@ -19,12 +12,4 @@ variable "ec2_instance_type" {
   description = "The AWS instance type to use"
   type        = string
   default     = "t2.micro"
-}
-
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "environment" {
-  default = "Yahtzee"
 }
