f8c26bfd0e493a3837c423daeeb96407d9900cf9
Dakota Schramm <william.schramm11@gmail.com>
2026-05-30 15:01:54 -0500

    Run app on port 3000

--- infra/terraform/modules/ec2/main.tf
+++ infra/terraform/modules/ec2/main.tf
@@ -31,9 +31,9 @@ resource "aws_security_group" "web" {
   }
 
   ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
+    description = "App"
+    from_port   = 3000
+    to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
