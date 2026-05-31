503cd6639dfc55de797ce6b292702b7d5c48fc15
Dakota Schramm <william.schramm11@gmail.com>
2026-05-30 22:39:13 -0500

    Change to use ipv6 addresses for ssh

--- infra/terraform/modules/ec2/main.tf
+++ infra/terraform/modules/ec2/main.tf
@@ -23,11 +23,11 @@ resource "aws_security_group" "web" {
   vpc_id = var.vpc_id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = [var.local_ssh_ip]  # Restrict to your IP
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    ipv6_cidr_blocks = [var.local_ssh_ip]  # Restrict to your IP
   }
 
   ingress {
